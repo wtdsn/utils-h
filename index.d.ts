032dc45725911c4bf28649a17d89780466a1a70f
黄万涛 <2747964040@qq.com>
2023-02-20 09:58:02 +0800

    增加ts声明

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,35 @@
 export function debounce(wait: number, fun: Function, immediate?: boolean): Function
 export function throttle(dur: number, fun: Function, endCall?: boolean): Function
+
+export interface LazyLoad {
+  root: string,
+  observe: () => void,
+  disconnect: () => void,
+  private _init: () => void,
+  private imgList: Element[],
+  private _cb: () => void
+  private rootEle: Element
+}
+
+
+interface parseResInter {
+  url: string,
+  scheme: string,
+  domain: string,
+  port: string,
+  path: string,
+  query: string,
+  hash: string,
+}
+
+interface queryInter {
+  [k: string]: string
+}
+
+
+
+export interface qs {
+  parse(url: string): parseResInter | {},
+  getQuery(url: string): queryInter,
+  setQuery(url?: string, query: queryInter): string
+}
--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-/* import qs from './dist/qs'
-import LazyLoad from './dist/lazyLoad' */
 import debounce from './dist/debounce'
 import throttle from './dist/throttle'
-
-/* export default {
-  qs,
-  LazyLoad,
-  throttle,
-  debounce
-} */
 
 export {
   debounce,
