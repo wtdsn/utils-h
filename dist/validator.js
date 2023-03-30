dda8043bd9f405777c9f1f0820613c516f0856f1
黄万涛 <2747964040@qq.com>
2023-03-30 17:11:08 +0800

    完善 validator

--- dist/validator.js
+++ dist/validator.js
@@ -1,18 +1,92 @@
 class Validator {
   constructor() {
     this.rules = []
+    this.ini()
   }
 
-  checkEmpty(text) {
+  ini() {
+    if (!Validator.prototype.inited) {
+      let keys = Object.keys(rules_h)
+      keys.forEach(v => {
+        Validator.prototype[v] = rules_h[v]
+      })
+      Validator.prototype.inited = true
+    }
+  }
+
+
+  addRule(rule) {
+    this.rules.push(rule)
+    return this.rule.length - 1
+  }
+  /* 
+    deleteRule(id) {
+      this.rules.splice(id, 1)
+    } */
+
+  clear() {
+    this.rules = []
+  }
+
+  // rule can be id , or inner rule
+  checkByRule(rule, ...args) {
+    if (typeof rule === 'number') {
+      return this.rules[rule](...args)
+    }
+
+    if (typeof rule === 'string') {
+      return rules_h[rule](...args)
+    }
+
+    return false
+  }
+
+  checkByRules(rules = []) {
+    return new Promise((resolve, reject) => {
+      rules.every((v) => {
+        if (this.checkByRule(v.rule, ...v.args)) { return true }
+        reject(v.errText)
+      })
+      resolve(true)
+    })
+  }
+}
+
+
+//  TODO add： 日期 QQ ， IP ， URL， 身份证，地址
+const rules_h = {
+  phone(text) {
+    return /^1[0-9]{10}$/.test(text)
+  },
+
+  mail(text) {
+    return /^[^\.-]([\w-]*\.?[\w-]+)+@[\w]+\.[A-z]+$/.test(text)
+  },
+
+  nameCh(text) {
+    return /^([\u4e00-\u9fa5]+·?[\u4e00-\u9fa5]+)+$/.test(text)
+  },
+
+  pw(text, strength = 1) {
+    if (/\s/.test(text)) return false
+    if (strength === 3) {
+      return /[A-z]/.test(text) && /[0-9]/.test(text) && /[\!@\#\$\%\^\&\*\(\)\_\+]/.test(text) && /[\w\!@\#\$\%\^\&\*\(\)\_\+]{8,}/.test(text)
+    } else if (strength === 2) {
+      return /[A-z]/.test(text) && /[0-9]/.test(text) && /[\w\!\@\#\$\%\^\&\*\(\)\_\+]{8,}/.test(text)
+    } else
+      return /[\w\!\@\#\$\%\^\&\*\(\)\_\+]{6,}/.test(text)
+  },
+
+  noEmpty(text) {
     return !!(text && text.trim())
-  }
+  },
 
-  checkEqual(text1, text2) {
+  equal(text1, text2) {
     // 直接比较
     return text1 === text2
-  }
+  },
 
-  checkObjEqual(obj1, obj2, keys = []) {
+  objEqual(obj1, obj2, keys = []) {
     // 浅比较对象
     if (keys.length === 0) {
       keys = Object.keys(obj1)
@@ -26,18 +100,19 @@ class Validator {
         return true
       }) && v1 === v2
     })
-  }
+  },
 
-  checkLen(text, minL = -Infinity, maxL = Infinity) {
+  len(text, minL = -Infinity, maxL = Infinity) {
     text = text.trim()
     // 仅设置了 minL
     if (minL !== -Infinity && maxL === Infinity) {
       maxL = minL
     }
     return text.length >= minL && text.length <= maxL
-  }
+  },
 
-  checkRange(num, min = -Infinity, max = Infinity, loose = false) {
+
+  range(num, min = -Infinity, max = Infinity, loose = false) {
     // if loose is true , num can be 'number string'
     if (typeof num === 'number') {
       return num >= min && num <= max
@@ -49,9 +124,9 @@ class Validator {
     }
 
     return false
-  }
+  },
 
-  checkIsNum(num, loose = false, canbeNaN = false) {
+  isNum(num, loose = false, canbeNaN = false) {
     // if loose is true , it can be 'number string'
     // if canbeNaN is true , NaN will return true
     if (typeof num === 'number') {
@@ -63,61 +138,10 @@ class Validator {
     }
     return false
   }
-
-  addRule(rule) {
-    this.rules.push(rule)
-    return this.rule.length - 1
-  }
-
-  deleteRule(id) {
-    this.rules.splice(id, 1)
-  }
-
-  clear() {
-    this.rules = []
-  }
-
-  // rule can be id , or inner rule
-  checkByRule(rule, obj, options) {
-    if (typeof rule === 'number') {
-      return this.rules[rule](obj, options)
-    }
-
-    if (typeof rule === 'string') {
-      return rules_h[rule](obj, options)
-    }
-
-    return false
-  }
 }
 
 
-//  TODO add： 日期，密码强弱 ， QQ ， IP ， URL， 身份证，地址
-const rules_h = {
-  phone(text) {
-    return /^1[0-9]{10}$/.test(text)
-  },
 
-  mail(text) {
-    return /^[^\.-]([\w-]*\.?[\w-]+)+@[\w]+\.[A-z]+$/.test(text)
-  },
 
-  nameCh(text) {
-    return /^([\u4e00-\u9fa5]+\.?[\u4e00-\u9fa5]+)+$/.test(text)
-  },
-
-  pw(text, strength = 1) {
-    if (/\s/.test(text)) return false
-    if (strength === 3) {
-      return /[A-z]/.test(text) && /[0-9]/.test(text) && /[\!@\#\$\%\^\&\*\(\)\_\+]/.test(text) && /[\w\!@\#\$\%\^\&\*\(\)\_\+]{8,}/.test(text)
-    } else if (strength === 2) {
-      return /[A-z]/.test(text) && /[0-9]/.test(text) && /[\w\!\@\#\$\%\^\&\*\(\)\_\+]{8,}/.test(text)
-    } else
-      return /[\w\!\@\#\$\%\^\&\*\(\)\_\+]{6,}/.test(text)
-  }
-}
-
-export default Validator
-// module.exports = Validator
-
-// let validator = new Validator()
+// export default Validator
+module.exports = Validator
